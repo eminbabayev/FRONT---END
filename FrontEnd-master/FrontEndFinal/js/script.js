f281b9cf005e5041ff05bb8367e84d617382b08e
emin <emin_babayev99@mail.ru>
2018-03-25 23:15:56 +0400

    Updated

---
+++ FrontEnd-master/FrontEndFinal/js/script.js
@@ -0,0 +1,129 @@
+$(document).ready(function(){
+
+
+ new WOW().init();
+
+     	//Hide Loading Box (Preloader)
+       function handlePreloader() {
+        if($('.preloader').length){
+          $('.preloader').delay(200).fadeOut(500);
+        }
+        }
+       
+            handlePreloader();
+
+if ($(window).scrollTop() > 20) {
+    $("#back-to-top").css("animation-name", "scroll-btn-show");
+  }
+  else {
+    $("#back-to-top").css("animation-name", "scroll-btn-hidden");
+  }
+
+$("#back-to-top").click(function(){
+      $('html,body').animate({scrollTop:0},1900);
+      return false;
+    });
+
+
+
+      
+
+
+    $(window).scroll(function(){
+    
+        if ($(this).scrollTop() > 20) {
+    $("#back-to-top").css("animation-name", "scroll-btn-show");
+  }
+  else {
+    $("#back-to-top").css("animation-name", "scroll-btn-hidden");
+  }
+      
+    })
+    $(".search-icon").click(function(){
+      $(".form-container").toggle();
+
+    
+    }) ;
+  
+
+    // if($(window).scrollTop()>0){ 
+    //   $("nav").css({
+    //     "top":"0",
+    //     "transition":"all 0s"});}
+    
+    navbg();
+    $(window).resize(function(){
+      navbg();
+
+    })
+      $(".search-icon").click(function () {
+    $(".form-container").toggle();
+  })
+  navbg();
+  $(window).resize(function () {
+    navbg();
+  })
+     function navbg(e){
+      if($(window).width()>1140){
+        $("nav").css("margin-top","30px").removeClass("bg-dark");      
+        $(window).scroll(function(){
+          // console.log(e)  
+          if($(window).scrollTop()>0){
+            $("nav").css("margin-top","0").addClass("bg-dark");
+          }
+          else{
+            $("nav").css("margin-top","30px").removeClass("bg-dark");
+          }
+        })
+      }
+      else{
+        $("nav").css("margin-top","0").addClass("bg-dark");
+        
+      }
+    }
+    
+   
+    $(".mydropdown-toggle").mouseover(function(){
+      $("nav").css("overflo")
+    });
+    // $(".input-text").click(function(){
+    //     $(this).css("border", "2px solid red ");
+    // });
+    // $('.timer').countTo();
+
+    $(".card-button").click(function(){
+      $(this).css("color", "#23527c");
+
+    });
+    
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin: 10,
+      nav:false,
+      smartSpeed:950,
+      autoplay:true,
+      
+      
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:3
+          }
+      }
+  })
+
+  $("#fact").appear(function(){
+    $('.timer').countTo();
+  });
+
+
+
+  
+
+});
+    
