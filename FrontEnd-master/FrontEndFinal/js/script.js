c0977fde4678e30f6aa12451674cd3009e3b45b4
emin <emin_babayev99@mail.ru>
2018-03-31 12:35:21 +0400

    FOOPOPLPl

--- FrontEnd-master/FrontEndFinal/js/script.js
+++ FrontEnd-master/FrontEndFinal/js/script.js
@@ -128,10 +128,27 @@ $("#back-to-top").click(function(){
           }
       }
   })
+  if($(".fancybox").length) {
+		$(".fancybox").fancybox({
+			openEffect  : 'fade',
+            closeEffect : 'fade',
+            helpers : {
+				media : {}
+			}
+		});
+	}
 
   $("#fact").appear(function(){
     $('.timer').countTo();
   });
+
+  $('.slides').bxSlider({
+    auto: true,
+    mode:'fade',
+    pagerCustom:'.images',
+    nextText:[ '<span class="fa fa-caret-right"></span>' ],
+  prevText:[ '<span class="fa fa-caret-left"></span>' ]
+});
 
   
 
